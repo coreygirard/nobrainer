ef21749eba2780a2765b8a8c7c730c75308ab25d
Corey Girard <corey.r.girard@gmail.com>
2018-09-01 12:53:00 -0400

    Linting, some tests

--- nobrainer.py
+++ nobrainer.py
@@ -1,92 +1,91 @@
-from numpy import exp, array, random, dot
+import numpy as np
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
 
 class NeuronLayer():
     def __init__(self, n_neurons, n_inputs):
-        self.synaptic_weights = 2 * random.random((n_inputs, n_neurons)) - 1
+        self.synaptic_weights = 2 * np.random.random((n_inputs, n_neurons)) - 1
+
 
 class ShallowNetwork():
-    def __init__(self,data,seed=None):
+    def __init__(self, data, seed=None):
         if seed != None:
-            random.seed(seed)
-        self.setTrainingData(data)
-        self.layer = NeuronLayer(1,len(self.training_inputs[0]))
+            np.random.seed(seed)
+        self.set_training_data(data)
+        self.layer = NeuronLayer(1, len(self.training_inputs[0]))
 
-    def __sigmoid(self, x):
-        return 1 / (1 + exp(-x))
 
-    def __sigmoid_derivative(self, x):
-        return x * (1 - x)
+    def get_training_data(self):
+        return [[i, o] for i, o in zip(self.training_inputs, self.training_outputs)]
 
-    def getTrainingData(self):
-        return [[i,o] for i,o in zip(self.training_inputs,self.training_outputs)]
-
-    def setTrainingData(self,data):
-        self.training_inputs = array([e[0] for e in data])
-        self.training_outputs = array([[e[1] for e in data]]).T
+    def set_training_data(self, data):
+        self.training_inputs = np.array([e[0] for e in data])
+        self.training_outputs = np.array([[e[1] for e in data]]).T
 
     def train(self, iterations):
-        for t in range(iterations):
+        for _ in range(iterations):
             output = self.think(self.training_inputs)
             error = self.training_outputs - output
-            adjustment = dot(self.training_inputs.T, error * self.__sigmoid_derivative(output))
+            adjustment = np.dot(self.training_inputs.T, error * sigmoid_derivative(output))
             self.layer.synaptic_weights += adjustment
 
     def think(self, inputs):
-        return self.__sigmoid(dot(array(inputs), self.layer.synaptic_weights))
+        return sigmoid(np.dot(np.array(inputs), self.layer.synaptic_weights))
+
 
 class DeepNetwork():
-    def __init__(self,data,depth,neurons,seed=None):
+    def __init__(self, data, depth, neurons, seed=None):
         if seed != None:
-            random.seed(seed)
-        self.setTrainingData(data)
+            np.random.seed(seed)
+        self.set_training_data(data)
         self.layer = []
 
-        assert(depth > 1)
+        assert depth > 1
 
-        self.layer.append(NeuronLayer(neurons,len(self.training_inputs[0])))
-        for t in range(depth-2):
-            self.layer.append(NeuronLayer(neurons,neurons))
-        self.layer.append(NeuronLayer(1,neurons))
+        self.layer.append(NeuronLayer(neurons, len(self.training_inputs[0])))
+        for _ in range(depth-2):
+            self.layer.append(NeuronLayer(neurons, neurons))
+        self.layer.append(NeuronLayer(1, neurons))
 
-    def __sigmoid(self, x):
-        return 1 / (1 + exp(-x))
+    def get_training_data(self):
+        return [[i, o] for i, o in zip(self.training_inputs, self.training_outputs)]
 
-    def __sigmoid_derivative(self, x):
-        return x * (1 - x)
-
-    def getTrainingData(self):
-        return [[i,o] for i,o in zip(training_inputs,training_outputs)]
-
-    def setTrainingData(self,data):
-        self.training_inputs = array([e[0] for e in data])
-        self.training_outputs = array([[e[1] for e in data]]).T
+    def set_training_data(self, data):
+        self.training_inputs = np.array([e[0] for e in data])
+        self.training_outputs = np.array([[e[1] for e in data]]).T
 
     def train(self, iterations):
-        for n in range(iterations):
+        for _ in range(iterations):
             thoughts = self.study(self.training_inputs)
 
             e = len(self.layer)-1
-            layer_error = {e:self.training_outputs - thoughts[e]}
-            layer_delta = {e:layer_error[e] * self.__sigmoid_derivative(thoughts[e])}
+            layer_error = {e: self.training_outputs - thoughts[e]}
+            layer_delta = {e: layer_error[e] * sigmoid_derivative(thoughts[e])}
             for t in reversed(range(e)):
                 layer_error[t] = layer_delta[t+1].dot(self.layer[t+1].synaptic_weights.T)
-                layer_delta[t] = layer_error[t] * self.__sigmoid_derivative(thoughts[t])
+                layer_delta[t] = layer_error[t] * sigmoid_derivative(thoughts[t])
 
-            layer_adjustment = {0:self.training_inputs.T.dot(layer_delta[0])}
-            for t in range(1,len(thoughts)):
+            layer_adjustment = {0: self.training_inputs.T.dot(layer_delta[0])}
+            for t in range(1, len(thoughts)):
                 layer_adjustment[t] = thoughts[t-1].T.dot(layer_delta[t])
 
             for t in range(len(self.layer)):
                 self.layer[t].synaptic_weights += layer_adjustment[t]
 
     def study(self, inputs):
-        thoughts = [array(inputs)]
+        thoughts = [np.array(inputs)]
         for e in self.layer:
-            thoughts.append(self.__sigmoid(dot(thoughts[-1], e.synaptic_weights)))
+            thoughts.append(sigmoid(np.dot(thoughts[-1], e.synaptic_weights)))
         return thoughts[1:]
 
     def think(self, inputs):
-        iterative = array(inputs)
+        iterative = np.array(inputs)
         for e in self.layer:
-            iterative = self.__sigmoid(dot(iterative, e.synaptic_weights))
+            iterative = self.__sigmoid(np.dot(iterative, e.synaptic_weights))
         return iterative
--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+
+
 setup(
-  name = 'nobrainer',
-  packages = ['nobrainer'], # this must be the same as the name above
-  version = '0.1',
-  description = 'Very elegant, clean, and fairly inefficient neural networks for toy deep learning problems',
-  author = 'Corey Girard',
-  author_email = 'corey.r.girard@gmail.com',
-  url = 'https://github.com/crgirard/no-brainer', # use the URL to the github repo
-  download_url = 'https://github.com/crgirard/no-brainer/tarball/0.1', # I'll explain this in a second
-  keywords = ['deep learning', 'neural network', 'ann', 'nn', 'machine learning'], # arbitrary keywords
-  classifiers = [],
+    name='nobrainer',
+    packages=['nobrainer'],
+    version='0.1',
+    description='Very elegant, clean, and fairly inefficient neural networks for toy deep learning problems',
+    author='Corey Girard',
+    author_email='corey.r.girard@gmail.com',
+    url='https://github.com/coreygirard/nobrainer',
+    download_url='https://github.com/coreygirard/nobrainer/tarball/0.1',
+    keywords=['deep learning', 'neural network', 'ann', 'nn', 'machine learning'],
+    classifiers=[],
 )
