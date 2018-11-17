53536aa0f547861b006411b662fab9cb48f8ae55
Corey Girard <corey.r.girard@gmail.com>
2018-11-17 18:16:38 -0500

    Restructuring tests

--- nobrainer/main.py
+++ nobrainer/main.py
@@ -4,22 +4,22 @@ import numpy as np
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+
 def sigmoid_derivative(x):
     return x * (1 - x)
 
 
-class NeuronLayer():
+class NeuronLayer:
     def __init__(self, n_neurons, n_inputs):
         self.synaptic_weights = 2 * np.random.random((n_inputs, n_neurons)) - 1
 
 
-class ShallowNetwork():
+class ShallowNetwork:
     def __init__(self, data, seed=None):
         if seed != None:
             np.random.seed(seed)
         self.set_training_data(data)
         self.layer = NeuronLayer(1, len(self.training_inputs[0]))
-
 
     def get_training_data(self):
         return [[i, o] for i, o in zip(self.training_inputs, self.training_outputs)]
@@ -32,14 +32,16 @@ class ShallowNetwork():
         for _ in range(iterations):
             output = self.think(self.training_inputs)
             error = self.training_outputs - output
-            adjustment = np.dot(self.training_inputs.T, error * sigmoid_derivative(output))
+            adjustment = np.dot(
+                self.training_inputs.T, error * sigmoid_derivative(output)
+            )
             self.layer.synaptic_weights += adjustment
 
     def think(self, inputs):
         return sigmoid(np.dot(np.array(inputs), self.layer.synaptic_weights))
 
 
-class DeepNetwork():
+class DeepNetwork:
     def __init__(self, data, depth, neurons, seed=None):
         if seed != None:
             np.random.seed(seed)
@@ -49,7 +51,7 @@ class DeepNetwork():
         assert depth > 1
 
         self.layer.append(NeuronLayer(neurons, len(self.training_inputs[0])))
-        for _ in range(depth-2):
+        for _ in range(depth - 2):
             self.layer.append(NeuronLayer(neurons, neurons))
         self.layer.append(NeuronLayer(1, neurons))
 
@@ -64,16 +66,18 @@ class DeepNetwork():
         for _ in range(iterations):
             thoughts = self.study(self.training_inputs)
 
-            e = len(self.layer)-1
+            e = len(self.layer) - 1
             layer_error = {e: self.training_outputs - thoughts[e]}
             layer_delta = {e: layer_error[e] * sigmoid_derivative(thoughts[e])}
             for t in reversed(range(e)):
-                layer_error[t] = layer_delta[t+1].dot(self.layer[t+1].synaptic_weights.T)
+                layer_error[t] = layer_delta[t + 1].dot(
+                    self.layer[t + 1].synaptic_weights.T
+                )
                 layer_delta[t] = layer_error[t] * sigmoid_derivative(thoughts[t])
 
             layer_adjustment = {0: self.training_inputs.T.dot(layer_delta[0])}
             for t in range(1, len(thoughts)):
-                layer_adjustment[t] = thoughts[t-1].T.dot(layer_delta[t])
+                layer_adjustment[t] = thoughts[t - 1].T.dot(layer_delta[t])
 
             for t in range(len(self.layer)):
                 self.layer[t].synaptic_weights += layer_adjustment[t]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,23 +1,25 @@
-import nobrainer
 import numpy as np
-from hypothesis import given, assume
+from hypothesis import assume, given
 from hypothesis.strategies import floats, lists
+
+from nobrainer.main import *
 
 
 @given(lists(floats(min_value=-5, max_value=5)))
 def test_sigmoid_property_basics(data):
     data = np.array(data)
 
-    output = nobrainer.sigmoid(data)
+    output = sigmoid(data)
 
-    assert type(output) == type(np.array([1]))  # TODO: figure out a way to use isinstance
+    assert type(output) == type(
+        np.array([1])
+    )  # TODO: figure out a way to use isinstance
     for e in output:
         assert isinstance(e, float)
         assert 0 < e < 1
 
 
-@given(floats(min_value=-5, max_value=5),
-       floats(min_value=-5, max_value=5))
+@given(floats(min_value=-5, max_value=5), floats(min_value=-5, max_value=5))
 def test_sigmoid_property_increasing(i, j):
     assume(i != j)
 
@@ -25,41 +27,39 @@ def test_sigmoid_property_increasing(i, j):
         i, j = j, i
 
     data = np.array([i, j])
-    output = nobrainer.sigmoid(data)
+    output = sigmoid(data)
 
     assert output[0] < output[1]
 
 
-@given(floats(min_value=0, max_value=5),
-       floats(min_value=0, max_value=5))
+@given(floats(min_value=0, max_value=5), floats(min_value=0, max_value=5))
 def test_sigmoid_property_diminishing_positive(i, j):
     assume(j > 0)
 
-    a, b, c = i, i+j, i+j+j
+    a, b, c = i, i + j, i + j + j
 
     data = np.array([a, b, c])
-    output = nobrainer.sigmoid(data)
+    output = sigmoid(data)
     delta = [output[1] - output[0], output[2] - output[1]]
 
     assert delta[0] > delta[1]
 
 
-@given(floats(min_value=-5, max_value=0),
-       floats(min_value=0, max_value=5))
+@given(floats(min_value=-5, max_value=0), floats(min_value=0, max_value=5))
 def test_sigmoid_property_diminishing_negative(i, j):
     assume(j > 0)
 
-    a, b, c = i, i-j, i-j-j
+    a, b, c = i, i - j, i - j - j
 
     data = np.array([a, b, c])
-    output = nobrainer.sigmoid(data)
+    output = sigmoid(data)
     delta = [output[1] - output[0], output[2] - output[1]]
 
     assert delta[0] < delta[1]
 
 
 def test_NeuronLayer():
-    layer = nobrainer.NeuronLayer(13, 17)
+    layer = NeuronLayer(13, 17)
 
     assert layer.synaptic_weights.shape == (17, 13)
     for row in layer.synaptic_weights:
